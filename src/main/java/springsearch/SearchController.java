d830ced2b4187a84a02ae6e2247e72617f6f4327
Atharvk13 <kanungoatharv@gmail.com>
2026-03-31 11:18:21 +0530

    @PathVariable annotation usage

--- src/main/java/springsearch/SearchController.java
+++ src/main/java/springsearch/SearchController.java
@@ -1,6 +1,7 @@
 package springsearch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
@@ -21,6 +22,12 @@ public class SearchController
 		String url="https://www.google.com/search?q="+query;
 		rv.setUrl(url);
 		return rv;
+	}
+	@RequestMapping("/user/{userId}")
+	public String getUserDetails(@PathVariable("userId") int userId)
+	{
+		System.out.println("The userId is : "+userId);
+		return "home";
 	}
 
 }
